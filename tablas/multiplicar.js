df4c8fe28002e5e7dd3166af1b3a0dff62244c97
Addo1996 <addo-fran@hotmail.es>
2026-04-24 19:47:08 -0500

    agregar caja txt

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,9 +1,10 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedorTabla");
     let contenido = "";
+    let numero = Number(document.getElementById("txtNumero").value);
 
     for(let i = 1; i <= 10; i++){
-        contenido = contenido + "<div class='fila'>5 x " + i + " = " + (5 * i) + "</div>";
+        contenido = contenido + "<div class='fila'>" + numero + " x " + i + " = " + (numero * i) + "</div>";
     }
 
     contenedor.innerHTML = contenido;
