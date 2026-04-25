f103a726f9ec926b5cb8137957600e463b531dd5
Addo1996 <addo-fran@hotmail.es>
2026-04-24 19:30:51 -0500

    tabla del 5

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -3,7 +3,7 @@ function generarTablas(){
     let contenido = "";
 
     for(let i = 1; i <= 10; i++){
-        contenido = contenido + "<div class='fila'>3 x " + i + " = " + (3 * i) + "</div>";
+        contenido = contenido + "<div class='fila'>5 x " + i + " = " + (5 * i) + "</div>";
     }
 
     contenedor.innerHTML = contenido;
